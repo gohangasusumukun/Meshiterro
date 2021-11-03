e0c84c8a69a6c3e3a74532e6e454dbd218ad1b6b
gohangasusumukun <isecha.ocha.cha.sket@gmail.com>
2021-11-03 02:54:00 +0000

    [add] コメント・いいね機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'aa8102faff47672aad15c83b593f986ecb257974ea93628431d8e4d86231de3597abb022190ae895b4bdfc56bf49132d4b36c6c8c0ceb8df88ffff664a2800b9'
